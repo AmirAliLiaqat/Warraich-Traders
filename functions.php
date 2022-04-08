4593e896e9066ddc82b10a5c3c8122ed09d6bbe1
Amir Ali <amirliaqat2020@gmail.com>
2022-04-08 12:26:00 +0500

    Adding Styles And Scripts

--- functions.php
+++ functions.php
@@ -4,4 +4,15 @@
  *
  * @package Warraich Traders
 */
+
+function warraich_trader_enqueue_scripts() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/assets/bootstrap/css/bootstrap.min.css' );
+
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'jQuery', get_template_directory_uri() . '/assets/js/jQuery.js' );
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/assets/js/script.js' );
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/bootstrap/js/bootstrap.min.js' );
+}
+add_action('wp_enqueue_scripts', 'warraich_trader_enqueue_scripts');
 ?>
