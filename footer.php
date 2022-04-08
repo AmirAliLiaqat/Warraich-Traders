33200f520d420bd27a085562a560c52bb06b0877
Amir Ali <amirliaqat2020@gmail.com>
2022-04-08 12:25:33 +0500

    Adding Footer

--- footer.php
+++ footer.php
@@ -6,7 +6,65 @@
 */
 ?>
 
-
+    <!-- Footer Start -->
+    <footer class="bg_img">
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-md-4">
+                    <h3 class="mb-3">
+                        <img src="images/logo.png" style="width: 85px; height: 75px;">
+                    </h3>
+                    <p>
+                        We have completed 5 projects, each project consists of around 50 houses.
+                    </p>
+                    <p>
+                        We’re a challenge driven team that loves to build meaningful living spaces with people
+                        that share the same passion and values as we have.
+                    </p>
+                </div><!--col-md-4-->
+                <div class="col-md-4">
+                    <h3 class="text-white mb-5">Contact Us</h3>
+                    <div class="d-flex">
+                        <i class="fa fa-map-marker my-1 text-secondary"></i>&nbsp;&nbsp;
+                        <p>Near 5 No.Bus Stop, Main Ferozpur Road, Kahna Nau, Lahore, Pakistan</p>
+                    </div>
+                    <div class="d-flex">
+                        <i class="fa fa-phone my-1 text-secondary"></i>&nbsp;&nbsp;
+                        <p>0300-4516820</p>
+                    </div>
+                    <div class="d-flex">
+                        <i class="fa fa-envelope my-1 text-secondary"></i>&nbsp;&nbsp;
+                        <p>ch_imtiazali@yahoo.com</p>
+                    </div>
+                    <div class="d-flex">
+                        <i class="fa fa-clock my-1 text-secondary"></i>&nbsp;&nbsp;
+                        <p>Mon - Sun : 09 AM - 09 PM</p>
+                    </div>
+                </div><!--col-md-4-->
+                <div class="col-md-4">
+                    <h3 class="text-white mb-5">Recent Photos</h3>
+                    <ul class="list-unstyled">
+                        <li><a href=""><img src="images/gallery1.jpg"></a></li>
+                        <li><a href=""><img src="images/gallery2.jpg"></a></li>
+                        <li><a href=""><img src="images/gallery3.jpg"></a></li>
+                        <li><a href=""><img src="images/gallery4.jpg"></a></li>
+                        <li><a href=""><img src="images/gallery5.jpg"></a></li>
+                        <li><a href=""><img src="images/gallery6.jpg"></a></li>
+                    </ul>
+                </div><!--col-md-4-->
+            </div><!--row-->
+            <hr class="text-secondary">
+            <div class="col-12 d-flex justify-content-center copyright_text">
+                <p>
+                    © 2022 Warraich Traders. All Rights Reserved. 
+                    <br>
+                    Designed and Developed by 
+                    <a href="https://bytebunch.com" target="_blank" class="text-decoration-none text-white">ByteBunch</a>
+                </p>
+            </div>
+        </div><!--container-->
+    </footer>
+    <!-- Footer End -->
 
 </body>
 </html>
