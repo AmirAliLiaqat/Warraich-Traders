d03d16f489b500f8ad7a22c8e4b02d9a553e0137
Amir Ali <amirliaqat2020@gmail.com>
2022-04-12 13:30:58 +0500

    Adding Widgets Areas

--- functions.php
+++ functions.php
@@ -38,5 +38,92 @@ function warraich_traders_enqueue_scripts() {
     wp_enqueue_script( 'script', get_template_directory_uri() . '/assets/js/script.js' );
     wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/bootstrap/js/bootstrap.min.js' );
 }
-add_action('wp_enqueue_scripts', 'warraich_traders_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'warraich_traders_enqueue_scripts'); 
+ 
+/************** Function using for adding sidebar widgets **************/
+function warraich_traders_widgets_init() {
+    register_sidebar( array(
+        'name'                =>  __( 'Banner Upper Section', 'warraich_traders' ),
+        'id'                     =>  'section-text-1',
+        'before_widget' =>  '<p id="%1$s" class="widget %2$s">',
+        'after_widget'   =>  '<p>',
+        'before_title'    =>  '<h2 class="widget-title">',
+        'after_title'      =>  '</h2>',
+    ) );
+ 
+    register_sidebar( array(
+        'name'                =>  __( 'Banner Under Section', 'warraich_traders' ),
+        'id'                     =>  'section-text-2',
+        'before_widget' =>  '<p id="%1$s" class="widget %2$s">',
+        'after_widget'   =>  '<p>',
+        'before_title'    =>  '<h2 class="widget-title">',
+        'after_title'      =>  '</h2>',
+    ) );
+ 
+    register_sidebar( array(
+        'name'                =>  __( 'Section Widget 1', 'warraich_traders' ),
+        'id'                     =>  'section-1',
+        'before_widget' =>  '<p id="%1$s" class="widget %2$s">',
+        'after_widget'   =>  '<p>',
+        'before_title'    =>  '<h4 class="widget-title">',
+        'after_title'      =>  '</h4>',
+    ) );
+ 
+    register_sidebar( array(
+        'name'                =>  __( 'Section Widget 2', 'warraich_traders' ),
+        'id'                     =>  'section-2',
+        'before_widget' =>  '<div><a id="%1$s" class="tags %2$s">',
+        'after_widget'   =>  '</a></div>',
+        'before_title'    =>  '<h4 class="widget-title">',
+        'after_title'      =>  '</h4>',
+    ) );
+ 
+    register_sidebar( array(
+        'name'                =>  __( 'Section Widget 3', 'warraich_traders' ),
+        'id'                     =>  'section-3',
+        'before_widget' =>  '<p id="%1$s" class="widget %2$s">',
+        'after_widget'   =>  '</p>',
+        'before_title'    =>  '<h4 class="widget-title">',
+        'after_title'      =>  '</h4>',
+    ) );
+ 
+    register_sidebar( array(
+        'name'                =>  __( 'Footer Widget 1', 'warraich_traders' ),
+        'id'                     =>  'footer-1',
+        'before_widget' =>  '<p id="%1$s" class="widget %2$s">',
+        'after_widget'   =>  '<p>',
+        'before_title'    =>  '<h3 class="widget-title">',
+        'after_title'      =>  '</h3>',
+    ) );
+ 
+    register_sidebar( array(
+        'name'                =>  __( 'Footer Widget 2', 'warraich_traders' ),
+        'id'                     =>  'footer-2',
+        'before_widget' =>  '<div><a id="%1$s" class="tags %2$s">',
+        'after_widget'   =>  '</a></div>',
+        'before_title'    =>  '<h3 class="widget-title">',
+        'after_title'      =>  '</h3>',
+    ) );
+ 
+    register_sidebar( array(
+        'name'                =>  __( 'Footer Widget 3', 'warraich_traders' ),
+        'id'                     =>  'footer-3',
+        'before_widget' =>  '<p id="%1$s" class="widget %2$s">',
+        'after_widget'   =>  '</p>',
+        'before_title'    =>  '<h3 class="widget-title">',
+        'after_title'      =>  '</h3>',
+    ) );
+}
+add_action('widgets_init', 'warraich_traders_widgets_init');
+
+// // TGM Plugin Activation
+// require_once get_template_directory() . '/inc/class-tgm-plugin-activation.php';
+// require get_template_directory() . '/inc/install-plugins.php';
+
+// // Kirki Embedded
+// require get_template_directory() . '/inc/kirki-config.php';
+
+// Adding Custom Post Types
+require get_template_directory() . '/inc/projects.php';
+
 ?>
