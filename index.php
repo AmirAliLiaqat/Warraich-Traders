931a4bd37a38112ccaec248c6456675c3284fb0d
Amir Ali <amirliaqat2020@gmail.com>
2022-04-12 17:08:46 +0500

    Making Home Page Dynamic

--- index.php
+++ index.php
@@ -8,87 +8,72 @@ get_header();
 ?>
 
     <!-- Section Start -->
-    <section>
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div><!--carousel-indicators-->
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/banner1.jpg" class="d-block w-100" style="height: 75vh;">
-                    <div class="carousel-caption d-none d-md-block" style="bottom: 50%;">
-                        <h1>If you want God to bless you, bless others.</h1>
-                    </div><!--carousel-caption-->
-                </div><!--carousel-item-->
-                <div class="carousel-item">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/banner2.jpg" class="d-block w-100" style="height: 75vh;">
-                    <div class="carousel-caption d-none d-md-block" style="bottom: 50%;">
-                        <h1>Helping hands are better than praying lips.</h1>
-                    </div><!--carousel-caption-->
-                </div><!--carousel-item-->
-                <div class="carousel-item">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/banner3.jpg" class="d-block w-100" style="height: 75vh;">
-                    <div class="carousel-caption d-none d-md-block" style="bottom: 50%;">
-                        <h1>Be the light that helps others see</h1>
-                    </div><!--carousel-caption-->
-                </div><!--carousel-item-->
-            </div><!--carousel-inner-->
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div><!--carouselExampleIndicators-->
-    </section>
+    <?php if ( true == get_theme_mod( 'toggle_home_slider', 'on' ) ) : ?>
+        <section>
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div><!--carousel-indicators-->
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/banner1.jpg" class="d-block w-100" style="height: 75vh;">
+                        <div class="carousel-caption d-none d-md-block" style="bottom: 50%;">
+                            <h1>If you want God to bless you, bless others.</h1>
+                        </div><!--carousel-caption-->
+                    </div><!--carousel-item-->
+                    <div class="carousel-item">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/banner2.jpg" class="d-block w-100" style="height: 75vh;">
+                        <div class="carousel-caption d-none d-md-block" style="bottom: 50%;">
+                            <h1>Helping hands are better than praying lips.</h1>
+                        </div><!--carousel-caption-->
+                    </div><!--carousel-item-->
+                    <div class="carousel-item">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/banner3.jpg" class="d-block w-100" style="height: 75vh;">
+                        <div class="carousel-caption d-none d-md-block" style="bottom: 50%;">
+                            <h1>Be the light that helps others see</h1>
+                        </div><!--carousel-caption-->
+                    </div><!--carousel-item-->
+                </div><!--carousel-inner-->
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div><!--carouselExampleIndicators-->
+        </section>
+    <?php endif; ?>
     <!-- Section End -->
 
     <!-- Section Start -->
     <section class="py-5">
         <div class="container">
             <div class="row">
-                <div class="col-12">
-                    <h2 class="text-center">WHy Choose Us?</h2>
-                    <div class="text-center">
-						<p style="max-width:65%; margin:0 auto;">
-                            Warraich traders was founded upon integrity, strong work ethic, our abilities, our people and our partners to provide our clients with the highest satisfaction.
-                        </p> 
-					</div><!--text-center-->
+                <div class="col-12 text-center mx-auto" style="max-width:65%;">
+                    <?php dynamic_sidebar('section-text-1'); ?>
                 </div><!--col-12-->
             </div><!--row-->
             <div class="row my-5">
                 <div class="col-4">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/experience-icon.png">
-                    <h4 class="my-3 text-secondary">10+ YEARS EXPERIENCE</h4>
-                    <p>
-                        Over the course of 10 years, we have built strong and trusting relationships with our clients, subcontractors and suppliers. We understand that partnering at every level is the key to a successful project.
-                    </p>
+                    <?php dynamic_sidebar('section-1'); ?>
                 </div><!--col-4-->
                 <div class="col-4">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/services.png">
-                    <h4 class="my-3 text-secondary">PROFESSIONAL SERVICES</h4>
-                    <p>
-                        With over 10 years experience & real focus on the customer satisfaction, you can found less like that our company structure and quality.
-                    </p>
+                    <?php dynamic_sidebar('section-2'); ?>
                 </div><!--col-4-->
                 <div class="col-4">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/good-material-icon.png">
-                    <h4 class="my-3 text-secondary">GOOD MATERIALS</h4>
-                    <p>
-                        We are always try to use best materials, you know that good quality materials highly cost but we minimize your cost.
-                    </p>
+                    <?php dynamic_sidebar('section-3'); ?>
                 </div><!--col-4-->
             </div><!--row-->
         </div><!--container-->
     </section>
     <!-- Section End -->
 
-    <!-- Section Start -->
-    <section class="bg_index bg_img">
+     <!-- Section Start -->
+     <section class="bg_index bg_img">
         <div class="container">
             <div class="row text-white py-5">
                 <div class="col-12">
@@ -203,22 +188,9 @@ get_header();
     <section>
         <div class="container">
             <div class="row py-5">
-                <div class="col-12">
-                    <h2 class="text-center">Our Services</h2>
-                    <div class="text-center">
-						<p class="" style="max-width:50%; margin:0 auto;">
-                            At Warraich Traders, performance and service are vitally important. We deliver efficient, timely and competitively low priced service.
-                        </p> 
-                        <p class="my-4" style=" margin:0 auto;">
-                            We don’t believe that quality has to suffer in order to receive a good 
-                            price. Quality workmanship at a fair price is what we strive to provide. 
-                            We have spent years building valuable relationships with loyal suppliers 
-                            and subcontractors. Thus the volume discounts we receive are reflected 
-                            in our low pricing. The diversity of our project base is a distinguishing 
-                            feature of our company. We take pride in our experienced, 
-                            knowledgeable, and dedicated staff, as well as, our ability to use innovated construction techniques to complete projects at a lower cost.
-                        </p> 
-					</div><!--text-center-->
+                <!-- <div class="col-12"> -->
+                <div class="col-12 text-center mx-auto">
+                    <?php dynamic_sidebar('section-text-2'); ?>
                 </div><!--col-12-->
             </div><!--row-->
         </div><!--container-->
