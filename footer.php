b69717d750e50cd28645d7f9f84f90622142e79f
Amir Ali <amirliaqat2020@gmail.com>
2022-04-12 17:07:01 +0500

    Making Footer Dynamic

--- footer.php
+++ footer.php
@@ -11,57 +11,28 @@
         <div class="container py-5">
             <div class="row">
                 <div class="col-md-4">
-                    <h3 class="mb-3">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" style="width: 85px; height: 75px;">
-                    </h3>
-                    <p>
-                        We have completed 5 projects, each project consists of around 50 houses.
-                    </p>
-                    <p>
-                        We’re a challenge driven team that loves to build meaningful living spaces with people
-                        that share the same passion and values as we have.
-                    </p>
+                    <?php dynamic_sidebar('footer-1'); ?>
                 </div><!--col-md-4-->
                 <div class="col-md-4">
-                    <h3 class="text-white mb-5">Contact Us</h3>
-                    <div class="d-flex">
-                        <i class="fa fa-map-marker my-1 text-secondary"></i>&nbsp;&nbsp;
-                        <p>Near 5 No.Bus Stop, Main Ferozpur Road, Kahna Nau, Lahore, Pakistan</p>
-                    </div>
-                    <div class="d-flex">
-                        <i class="fa fa-phone my-1 text-secondary"></i>&nbsp;&nbsp;
-                        <p>0300-4516820</p>
-                    </div>
-                    <div class="d-flex">
-                        <i class="fa fa-envelope my-1 text-secondary"></i>&nbsp;&nbsp;
-                        <p>ch_imtiazali@yahoo.com</p>
-                    </div>
-                    <div class="d-flex">
-                        <i class="fa fa-clock my-1 text-secondary"></i>&nbsp;&nbsp;
-                        <p>Mon - Sun : 09 AM - 09 PM</p>
-                    </div>
+                    <?php dynamic_sidebar('footer-2'); ?>
                 </div><!--col-md-4-->
                 <div class="col-md-4">
-                    <h3 class="text-white mb-5">Recent Photos</h3>
-                    <ul class="list-unstyled">
-                        <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/gallery1.jpg"></a></li>
-                        <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/gallery2.jpg"></a></li>
-                        <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/gallery3.jpg"></a></li>
-                        <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/gallery4.jpg"></a></li>
-                        <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/gallery5.jpg"></a></li>
-                        <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/gallery6.jpg"></a></li>
-                    </ul>
+                    <?php dynamic_sidebar('footer-3'); ?>
                 </div><!--col-md-4-->
             </div><!--row-->
-            <hr class="text-secondary">
-            <div class="col-12 d-flex justify-content-center copyright_text">
-                <p>
-                    © 2022 Warraich Traders. All Rights Reserved. 
-                    <br>
-                    Designed and Developed by 
-                    <a href="https://bytebunch.com" target="_blank" class="text-decoration-none text-white">ByteBunch</a>
-                </p>
-            </div>
+
+            <?php if ( true == get_theme_mod( 'toggle_footer_copyright', 'on' ) ) : ?>
+                <hr class="text-secondary">
+                <div class="col-12 d-flex justify-content-center copyright_text">
+                    <p>
+                        <?php 
+                            echo get_theme_mod( 'footer_copyright_text', '© 2022 Warraich Traders. All Rights Reserved.' . '<br>' . 'Designed and Developed by 
+                            <a href="https://bytebunch.com" target="_blank" class="text-decoration-none text-white">ByteBunch</a>' ) 
+                        ?>
+                    </p>
+                </div><!--col-12-->
+            <?php endif; ?>
+            
         </div><!--container-->
     </footer>
     <!-- Footer End -->
