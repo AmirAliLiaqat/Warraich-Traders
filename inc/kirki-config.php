301f007d494e2edc45a368b54b0a27bec0382c5f
Amir Ali <amirliaqat2020@gmail.com>
2022-04-12 17:07:45 +0500

    Adding Customizer Fields

--- inc/kirki-config.php
+++ inc/kirki-config.php
@@ -2,39 +2,18 @@
 /**
  * Customizer Config.
  *
- * @package ByteBunch
+ * @package Warraich Traders
 */
 Kirki::add_config( 'bytebunch_options', array(
     'capability' => 'edit_theme_options',
     'options_type' => 'theme_mod',
 ) );
 
-Kirki::add_panel( 'header_settings', array(
-    'priority'    => 10,
-    'title'       => esc_html__( 'Header Settings', 'kirki' ),
-    'description' => esc_html__( 'Manage Header & Menu Layout', 'kirki' ),
-    'icon'    => 'dashicons-admin-tools',
-) );
-
-Kirki::add_section( 'header_layout_settings', array(
-    'title'       => esc_html__( 'Layout', 'kirki' ),
-    'description' => esc_html__( 'Manage Header Layout', 'kirki' ),
-    'panel'       => 'header_settings',
-    'priority'    => 160,
-) );
-
 Kirki::add_section( 'menu_settings', array(
     'title'       => esc_html__( 'Menu Settings', 'kirki' ),
     'description' => esc_html__( 'Manage Menu Layout & Settings', 'kirki' ),
-    'panel'       => 'header_settings',
-    'priority'    => 160,
-) );
-
-Kirki::add_section( 'typography_settings', array(
-    'title'       => esc_html__( 'Typography', 'kirki' ),
-    'description' => esc_html__( 'Text Styles & Options', 'kirki' ),
+    'icon'    => 'dashicons-admin-tools',
     'priority'    => 10,
-    'icon'    => 'dashicons-editor-spellcheck',
 ) );
 
 Kirki::add_section( 'homepage_settings', array(
@@ -44,6 +23,13 @@ Kirki::add_section( 'homepage_settings', array(
     'icon'    => 'dashicons-layout',
 ) );
 
+Kirki::add_section( 'slider_settings', array(
+    'title'       => esc_html__( 'Slider Settings', 'kirki' ),
+    'description' => esc_html__( 'Manage Slider', 'kirki' ),
+    'priority'    => 10,
+    'icon'    => 'dashicons-admin-page',
+) );
+
 Kirki::add_section( 'footer_settings', array(
     'title'       => esc_html__( 'Footer Layout', 'kirki' ),
     'description' => esc_html__( 'Manage Footer Copyright Text', 'kirki' ),
@@ -51,14 +37,22 @@ Kirki::add_section( 'footer_settings', array(
     'icon'    => 'dashicons-edit',
 ) );
 
+Kirki::add_section( 'typography_settings', array(
+    'title'       => esc_html__( 'Typography', 'kirki' ),
+    'description' => esc_html__( 'Text Styles & Options', 'kirki' ),
+    'priority'    => 10,
+    'icon'    => 'dashicons-editor-spellcheck',
+) );
+
+/************************** Body background control ******************************/
 Kirki::add_field( 'bytebunch_options', [
     'type'            => 'background',
-    'settings'      => 'background_setting',
-    'label'           => esc_html__( 'Background Control', 'kirki' ),
-    'description' => esc_html__( 'Background conrols are pretty complex! (but useful if used properly)', 'kirki' ),
-    'section'       => 'header_layout_settings',
+    'settings'      => 'body_background_setting',
+    'label'           => esc_html__( 'Body Background Control', 'kirki' ),
+    'description' => esc_html__( 'Body Background conrols are pretty complex! (but useful if used properly)', 'kirki' ),
+    'section'       => 'homepage_settings',
     'default'       => [
-        'background-color'           => '#000',
+        'background-color'           => '#fff',
         'background-image'          => '',
         'background-repeat'         => 'repeat',
         'background-position'       => 'center center',
@@ -68,51 +62,28 @@ Kirki::add_field( 'bytebunch_options', [
     'transport' => 'auto',
     'output'      => [
         [
-            'element' => 'header',
+            'element' => 'body',
         ],
     ],
 ] );
 
+/************************** Home slider setting ******************************/
 Kirki::add_field( 'bytebunch_options', [
-    'type'            => 'background',
-    'settings'      => 'menu_background_setting',
-    'label'           => esc_html__( 'Menu Background', 'kirki' ),
-    'description' => esc_html__( 'Background controls are pretty complex! (but useful if used properly)', 'kirki' ),
-    'section'       => 'menu_settings',
-    'default'       => [
-        'background-color'           => '#000',
-        'background-image'          => '',
-        'background-repeat'         => 'repeat',
-        'background-position'       => 'center center',
-        'background-size'             => 'cover',
-        'background-attachment' => 'scroll',
-    ],
-    'transport' => 'auto',
-    'output'      => [
-        [
-            'element' => '.navbar-nav .nav-item',
-        ],
+    'type'    => 'switch',
+    'settings'    => 'toggle_home_slider',
+    'label'       => esc_html__( 'Switch Field', 'kirki' ),
+    'description' => esc_html__( 'Homepage Slider', 'kirki' ),
+    'section'     => 'slider_settings',
+    'default'     => '1',
+    'priority'    => 10,
+    'choices'     => [
+        'on'  => esc_html__( 'Enable', 'kirki' ),
+        'off' => esc_html__( 'Disable', 'kirki' ),
     ],
 ] );
 
-Kirki::add_field( 'bytebunch_options', [
-    'type'            => 'background',
-    'settings'      => 'menu_active_background_setting',
-    'label'           => esc_html__( 'Menu Active & Hover', 'kirki' ),
-    'description' => esc_html__( 'Background controls are pretty complex! (but useful if used properly)', 'kirki' ),
-    'section'       => 'menu_settings',
-    'default'       => [
-        'background-color'           => '#e55a21',
-        'background-image'          => '',
-    ],
-    'transport' => 'auto',
-    'output'      => [
-        [
-            'element' => '.navbar .container .collapse ul li:hover',
-        ],
-    ],
-] );
 
+/************************** Home typography ******************************/
 Kirki::add_field( 'bytebunch_options', [
     'type'    => 'typography',
     'settings'    => 'heading_settings',
@@ -122,13 +93,11 @@ Kirki::add_field( 'bytebunch_options', [
         'font-family'     => 'Roboto',
         'variant'         => 'regular',
         'font-style'      => 'normal',
-        // 'color'           => '#000',
-        'font-size'       => '1.4em',
+        'font-size'       => '2rem',
         'line-height'     => '0',
         'letter-spacing'  => '0',
         'text-transform'  => 'none',
         'text-decoration' => 'none',
-        // 'text-align'      => 'left',
     ],
     'choices' => [
         'fonts' => [
@@ -148,83 +117,60 @@ Kirki::add_field( 'bytebunch_options', [
     ],
 ] );
 
+/************************** Nav background control ******************************/
 Kirki::add_field( 'bytebunch_options', [
     'type'            => 'background',
-    'settings'      => 'button_background_setting',
-    'label'           => esc_html__( 'Button Background Control', 'kirki' ),
-    'description' => esc_html__( 'Change the all site buttons background color', 'kirki' ),
-    'section'       => 'homepage_settings',
+    'settings'      => 'background_setting',
+    'label'           => esc_html__( 'Menu Background Control', 'kirki' ),
+    'description' => esc_html__( 'Menu Background conrols are pretty complex! (but useful if used properly)', 'kirki' ),
+    'section'       => 'menu_settings',
     'default'       => [
-        'background-color'           => '#e55a21',
+        'background-color'           => '#fff',
+        'background-image'          => '',
+        'background-repeat'         => 'repeat',
+        'background-position'       => 'center center',
+        'background-size'             => 'cover',
+        'background-attachment' => 'scroll',
     ],
     'transport' => 'auto',
     'output'      => [
         [
-            'element' => '.wp-block-group__inner-container button, .orange-btn, input[type="submit"]',
+            'element' => 'nav',
         ],
     ],
 ] );
 
 Kirki::add_field( 'bytebunch_options', [
-    'type'            => 'background',
-    'settings'      => 'button_hover_background_setting',
-    'label'           => esc_html__( 'Button Hover Color Control', 'kirki' ),
-    'description' => esc_html__( 'Change the all site buttons hover color', 'kirki' ),
-    'section'       => 'homepage_settings',
-    'default'       => [
-        'background-color'           => '#000',
-    ],
+    'type'            => 'color',
+    'settings'      => 'menu_color_setting',
+    'label'           => esc_html__( 'Menu Color', 'kirki' ),
+    'description' => esc_html__( 'Manage the menu colors', 'kirki' ),
+    'section'       => 'menu_settings',
+    'default'       => '#000',
     'transport' => 'auto',
     'output'      => [
         [
-            'element' => '.wp-block-group__inner-container button:hover, .orange-btn:hover, input[type="submit"]:hover',
+            'element' => '.nav-links ul li a',
         ],
     ],
 ] );
 
 Kirki::add_field( 'bytebunch_options', [
-    'type'    => 'switch',
-    'settings'    => 'toggle_home_sidebar',
-    'label'       => esc_html__( 'Switch Field', 'kirki' ),
-    'description' => esc_html__( 'Homepage Sidebar', 'kirki' ),
-    'section'     => 'homepage_settings',
-    'default'     => '1',
-    'priority'    => 10,
-    'choices'     => [
-        'on'  => esc_html__( 'Enable', 'kirki' ),
-        'off' => esc_html__( 'Disable', 'kirki' ),
+    'type'            => 'color',
+    'settings'      => 'menu_color_hover_setting',
+    'label'           => esc_html__( 'Menu Hover Color', 'kirki' ),
+    'description' => esc_html__( 'Manage the menu hover colors', 'kirki' ),
+    'section'       => 'menu_settings',
+    'default'       => '#69bde7',
+    'transport' => 'auto',
+    'output'      => [
+        [
+            'element' => '.nav-links ul li a:hover',
+        ],
     ],
 ] );
 
-Kirki::add_field( 'bytebunch_options', [
-        'type'        => 'radio-image',
-        'setting'     => 'homepage_layout',
-        'label'       => __( 'Site Layout', 'superminimal' ),
-        'description' => __( 'Change The Homepage Layout', 'superminimal' ),
-        'section'     => 'homepage_settings',
-        'default'     => 'sidebar-right',
-        'priority'    => 10,
-        'choices'     => array(
-          'sidebar-left' => trailingslashit( get_template_directory_uri() ) . '/images/first-layout.png',
-          'fullwidth' => trailingslashit( get_template_directory_uri() ) . '/images/second-layout.png',
-          'sidebar-right' => trailingslashit( get_template_directory_uri() ) . '/images/third-layout.png',
-        ),
-] );
-
-Kirki::add_field( 'bytebunch_options', [
-    'type'    => 'switch',
-    'settings'    => 'toggle_footer_menu',
-    'label'       => esc_html__( 'Switch Field', 'kirki' ),
-    'description' => esc_html__( 'Footer Menu', 'kirki' ),
-    'section'     => 'footer_settings',
-    'default'     => '1',
-    'priority'    => 10,
-    'choices'     => [
-        'on'  => esc_html__( 'Enable', 'kirki' ),
-        'off' => esc_html__( 'Disable', 'kirki' ),
-    ],
-] );
-
+/************************** Footer copyright setting ******************************/
 Kirki::add_field( 'bytebunch_options', [
     'type'    => 'switch',
     'settings'    => 'toggle_footer_copyright',
@@ -244,7 +190,8 @@ Kirki::add_field( 'bytebunch_options', [
     'settings' => 'footer_copyright_text',
     'label'    => esc_html__( 'Footer Copyright', 'kirki' ),
     'section'  => 'footer_settings',
-    'default'  => esc_html__( '© Copyright 2021. Theme by ByteBunch.', 'kirki' ),
+    'default'  => esc_html__( '© 2022 Warraich Traders. All Rights Reserved.' . '<br>' . 'Designed and Developed by 
+    <a href="https://bytebunch.com" target="_blank" class="text-decoration-none text-white">ByteBunch</a>', 'kirki' ),
     'priority' => 10,
 ] );
 
