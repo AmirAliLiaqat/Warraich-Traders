a2c8fc489f4290b8e275600e74b087095fe2dae4
Amir Ali <amirliaqat2020@gmail.com>
2022-04-13 09:52:12 +0500

    Adding New Customizer Fields

--- inc/kirki-config.php
+++ inc/kirki-config.php
@@ -4,28 +4,38 @@
  *
  * @package Warraich Traders
 */
-Kirki::add_config( 'bytebunch_options', array(
+Kirki::add_config( 'warraich_traders_options', array(
     'capability' => 'edit_theme_options',
     'options_type' => 'theme_mod',
 ) );
 
-Kirki::add_section( 'menu_settings', array(
-    'title'       => esc_html__( 'Menu Settings', 'kirki' ),
-    'description' => esc_html__( 'Manage Menu Layout & Settings', 'kirki' ),
-    'icon'    => 'dashicons-admin-tools',
-    'priority'    => 10,
-) );
-
-Kirki::add_section( 'homepage_settings', array(
+Kirki::add_panel( 'homepage_settings', array(
     'title'       => esc_html__( 'Homepage Layout', 'kirki' ),
     'description' => esc_html__( 'Manage Homepage Layout', 'kirki' ),
     'priority'    => 10,
     'icon'    => 'dashicons-layout',
 ) );
 
+Kirki::add_section( 'body_layout_settings', array(
+    'title'       => esc_html__( 'Layout', 'kirki' ),
+    'description' => esc_html__( 'Manage Body Layout', 'kirki' ),
+    'panel'       => 'homepage_settings',
+    'priority'    => 10,
+    'icon'          => 'dashicons-image-filter',
+) );
+
+Kirki::add_section( 'menu_settings', array(
+    'title'       => esc_html__( 'Menu Settings', 'kirki' ),
+    'description' => esc_html__( 'Manage Menu Layout & Settings', 'kirki' ),
+    'panel'       => 'homepage_settings',
+    'priority'    => 10,
+    'icon'    => 'dashicons-admin-tools',
+) );
+
 Kirki::add_section( 'slider_settings', array(
     'title'       => esc_html__( 'Slider Settings', 'kirki' ),
     'description' => esc_html__( 'Manage Slider', 'kirki' ),
+    'panel'       => 'homepage_settings',
     'priority'    => 10,
     'icon'    => 'dashicons-admin-page',
 ) );
@@ -45,12 +55,12 @@ Kirki::add_section( 'typography_settings', array(
 ) );
 
 /************************** Body background control ******************************/
-Kirki::add_field( 'bytebunch_options', [
+Kirki::add_field( 'warraich_traders_options', [
     'type'            => 'background',
     'settings'      => 'body_background_setting',
     'label'           => esc_html__( 'Body Background Control', 'kirki' ),
     'description' => esc_html__( 'Body Background conrols are pretty complex! (but useful if used properly)', 'kirki' ),
-    'section'       => 'homepage_settings',
+    'section'       => 'body_layout_settings',
     'default'       => [
         'background-color'           => '#fff',
         'background-image'          => '',
@@ -68,7 +78,7 @@ Kirki::add_field( 'bytebunch_options', [
 ] );
 
 /************************** Home slider setting ******************************/
-Kirki::add_field( 'bytebunch_options', [
+Kirki::add_field( 'warraich_traders_options', [
     'type'    => 'switch',
     'settings'    => 'toggle_home_slider',
     'label'       => esc_html__( 'Switch Field', 'kirki' ),
@@ -82,9 +92,70 @@ Kirki::add_field( 'bytebunch_options', [
     ],
 ] );
 
+Kirki::add_field( 'warraich_traders_options', [
+    'type'    => 'text',
+    'settings' => 'slider_text_1',
+    'label'    => esc_html__( 'Slider Text Box 1', 'kirki' ),
+    'section'  => 'slider_settings',
+    'default'  => esc_html__( 'If you want God to bless you, bless others.', 'kirki' ),
+    'priority' => 10,
+] );
 
+Kirki::add_field( 'warraich_traders_options', [
+    'type'    => 'image',
+    'settings'    => 'slider_img_1',
+    'label'       => esc_html__( 'Slider Image 1', 'kirki' ),
+    // 'description' => esc_html__( 'The saved value will be an array.', 'kirki' ),
+    'section'     => 'slider_settings',
+    'default'     => '',
+    'choices'     => [
+        'save_as' => 'array',
+    ],
+] );
+
+Kirki::add_field( 'warraich_traders_options', [
+    'type'    => 'text',
+    'settings' => 'slider_text_2',
+    'label'    => esc_html__( 'Slider Text Box 2', 'kirki' ),
+    'section'  => 'slider_settings',
+    'default'  => esc_html__( 'Helping hands are better than praying lips.', 'kirki' ),
+    'priority' => 10,
+] );
+
+Kirki::add_field( 'warraich_traders_options', [
+    'type'    => 'image',
+    'settings'    => 'slider_img_2',
+    'label'       => esc_html__( 'Slider Image 2', 'kirki' ),
+    // 'description' => esc_html__( 'The saved value will be an array.', 'kirki' ),
+    'section'     => 'slider_settings',
+    'default'     => '',
+    'choices'     => [
+        'save_as' => 'array',
+    ],
+] );
+
+Kirki::add_field( 'warraich_traders_options', [
+    'type'    => 'text',
+    'settings' => 'slider_text_3',
+    'label'    => esc_html__( 'Slider Text Box 3', 'kirki' ),
+    'section'  => 'slider_settings',
+    'default'  => esc_html__( 'Be the light that helps others see', 'kirki' ),
+    'priority' => 10,
+] );
+
+Kirki::add_field( 'warraich_traders_options', [
+    'type'    => 'image',
+    'settings'    => 'slider_img_3',
+    'label'       => esc_html__( 'Slider Image 3', 'kirki' ),
+    // 'description' => esc_html__( 'The saved value will be an array.', 'kirki' ),
+    'section'     => 'slider_settings',
+    'default'     => '',
+    'choices'     => [
+        'save_as' => 'array',
+    ],
+] );
 /************************** Home typography ******************************/
-Kirki::add_field( 'bytebunch_options', [
+Kirki::add_field( 'warraich_traders_options', [
     'type'    => 'typography',
     'settings'    => 'heading_settings',
     'label'       => esc_html__( 'Headings', 'kirki' ),
@@ -118,7 +189,7 @@ Kirki::add_field( 'bytebunch_options', [
 ] );
 
 /************************** Nav background control ******************************/
-Kirki::add_field( 'bytebunch_options', [
+Kirki::add_field( 'warraich_traders_options', [
     'type'            => 'background',
     'settings'      => 'background_setting',
     'label'           => esc_html__( 'Menu Background Control', 'kirki' ),
@@ -140,7 +211,7 @@ Kirki::add_field( 'bytebunch_options', [
     ],
 ] );
 
-Kirki::add_field( 'bytebunch_options', [
+Kirki::add_field( 'warraich_traders_options', [
     'type'            => 'color',
     'settings'      => 'menu_color_setting',
     'label'           => esc_html__( 'Menu Color', 'kirki' ),
@@ -155,7 +226,7 @@ Kirki::add_field( 'bytebunch_options', [
     ],
 ] );
 
-Kirki::add_field( 'bytebunch_options', [
+Kirki::add_field( 'warraich_traders_options', [
     'type'            => 'color',
     'settings'      => 'menu_color_hover_setting',
     'label'           => esc_html__( 'Menu Hover Color', 'kirki' ),
@@ -171,7 +242,7 @@ Kirki::add_field( 'bytebunch_options', [
 ] );
 
 /************************** Footer copyright setting ******************************/
-Kirki::add_field( 'bytebunch_options', [
+Kirki::add_field( 'warraich_traders_options', [
     'type'    => 'switch',
     'settings'    => 'toggle_footer_copyright',
     'label'       => esc_html__( 'Switch Field', 'kirki' ),
@@ -185,7 +256,7 @@ Kirki::add_field( 'bytebunch_options', [
     ],
 ] );
 
-Kirki::add_field( 'bytebunch_options', [
+Kirki::add_field( 'warraich_traders_options', [
     'type'    => 'text',
     'settings' => 'footer_copyright_text',
     'label'    => esc_html__( 'Footer Copyright', 'kirki' ),
